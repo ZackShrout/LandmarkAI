e51715b8c57bbf8fc61927418dc5999a16599ba3
ZackShrout <zshrout@gmail.com>
2021-06-07 21:27:25 -0500

    Used JSON Utils to generate two classes to deserialize JSON string received from client post request

--- LandmarkAI/LandmarkAI/MainWindow.xaml.cs
+++ LandmarkAI/LandmarkAI/MainWindow.xaml.cs
@@ -57,6 +57,9 @@ namespace LandmarkAI
                 {
                     content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(contentType);
                     var response = await client.PostAsync(url, content);
+
+                    string responseString = await response.Content.ReadAsStringAsync();
+
                 }
 
             }
